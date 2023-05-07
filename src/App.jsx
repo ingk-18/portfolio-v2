9e125780457617d185fbce835900c8434482c9b0
ingk-18 <64448438+ingk-18@users.noreply.github.com>
2023-05-07 18:35:26 +0900

    背景に画像設定

--- src/App.jsx
+++ src/App.jsx
@@ -28,13 +28,17 @@ function App() {
         </div>
       </header>
 
-      <section className="text-gray-700" id="home">
+      <section
+        className="text-white min-h-screen bg-cover  object-cover flex flex-col justify-end"
+        style={{
+          backgroundImage: `url('./img/img4.jpg')`,
+        }}
+        id="home"
+      >
         <div className="container flex mx-auto py-20 px-5 flex-col md:flex-row items-center">
           <div className="md:w-1/2 flex-grow mb-16 lg:pr-24 md:pr-16 text-center md:text-left">
-            <h1 className="text-3xl sm:text-6xl text-gray-900 font-medium mb-4">
-              Hi!
-              <br />
-              I'm GAKY
+            <h1 className="text-3xl sm:text-6xl font-medium mb-4">
+              Introducing GAKY
               <br />
               Web Developer
             </h1>
@@ -45,12 +49,12 @@ function App() {
               temporibus necessitatibus labore provident inventore maxime cum
               aspernatur dolore.
             </p>
-            <button className="text-white bg-green-500 py-2 px-6 border-0 rounded text-lg hover:bg-green-600 duration-300">
+            {/* <button className="text-white bg-green-500 py-2 px-6 border-0 rounded text-lg hover:bg-green-600 duration-300">
               Contact
-            </button>
+            </button> */}
           </div>
           <div className="md:w-1/2 lg:max-w-lg flex justify-center">
-            <img src="./img/icon.jpg" alt="" />
+            {/* <img src="./img/icon.jpg" alt="" /> */}
           </div>
         </div>
       </section>
