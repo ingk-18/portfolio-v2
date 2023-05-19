b54b9ad2eeb51458cf625a6f8272b5cbd00bcf0e
ingk-18 <64448438+ingk-18@users.noreply.github.com>
2023-05-19 09:34:12 +0900

    各セクションコンポーネント化

---
+++ src/components/introduce.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const introduce = () => {
+  return (
+    <div>
+      <section className="text-gray-700 border-t border-gray-200 min-h-screen">
+        <div className="container flex py-24 px-5 mx-auto flex-wrap">
+          <div className="md:w-1/2 flex justify-center lg:pr-4 md:pr-2">
+            <img className="rounded-lg" src="./img/icon.jpg" alt="" />
+          </div>
+          <div className="md:w-1/2 font-serif lg:pl-4 md:pl-2">
+            <h1 className="text-3xl sm:text-6xl">GAKY</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+              molestiae explicabo tempora. Tempore, provident. Voluptates porro
+              quibusdam qui voluptate error! Consequuntur, quasi possimus nam
+              doloremque consectetur nostrum ipsa? Dolor perspiciatis voluptatem
+              asperiores, repudiandae omnis voluptatum, natus sed, voluptates ad
+              rerum cum dignissimos sint incidunt consequuntur consequatur
+              reiciendis excepturi laborum obcaecati voluptate culpa
+              reprehenderit praesentium ipsum? Reiciendis veritatis molestiae,
+              corporis quas nihil quaerat officia vero quo culpa iusto. Quos
+              odit eum ipsam est, natus repudiandae laborum, cupiditate
+              molestias dolores fugiat doloremque sint reprehenderit eveniet
+              iusto fuga accusantium nam beatae ut officia? Dicta nihil minus
+              hic quidem architecto! Culpa ullam fugit dolorum laudantium
+              voluptatum tempore deleniti accusamus saepe aperiam asperiores in
+              veniam quisquam officia ipsam, eum fuga assumenda sequi quia
+              excepturi ad temporibus, quos cumque repellat! Saepe molestiae ut
+              impedit, delectus tempore dolorem vel, beatae quam natus suscipit
+              porro recusandae ratione! Eligendi provident quis accusamus
+              quidem! Sint delectus tempore ratione explicabo maxime.
+              Repudiandae ut perspiciatis, optio aliquam hic at quas quo
+              molestiae, illum, saepe nobis obcaecati. Provident nostrum numquam
+              placeat fugiat hic sit reiciendis id veniam adipisci nam possimus
+              maxime blanditiis odio sint tempore expedita officia quibusdam
+              nemo, aperiam quia sed harum? At dolorum ullam ea laboriosam
+              consequuntur nesciunt corrupti porro sint.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default introduce;
